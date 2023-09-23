ee563dfe9cb72fc4162f53d93c7194994d0874af
rudi-1310 <rudrakshbchavda@gmail.com>
2023-09-23 12:30:26 +0530

    mid commit

--- src/Register.js
+++ src/Register.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom'
+
 export default function Register() {
     const navigate = useNavigate();
-    const [interest, setInterest] = useState('')
+    const [interests, setInterests] = useState([]);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [FirstName, setFirstName] = useState("");
@@ -18,23 +19,24 @@ export default function Register() {
                 password,
                 FirstName,
                 LastName,
-                interest
-            })
-            if (response.data === "exist") {
-                alert("User already exists")
+                interests
+            });
 
+            if (response.data === "exist") {
+                alert("User already exists");
             } else if (response.data === "success") {
                 NavigateVerify();
-                await axios.post('http://localhost:8000/verify', { email })
+                await axios.post('http://localhost:8000/verify', { email });
             }
-
         } catch (e) {
-            console.log(e);
+            console.error(e);
         }
     }
+
     const NavigateVerify = () => {
         navigate("/home", { state: { id: 'hi' } });
     }
+
     return (
         <div>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -45,39 +47,39 @@ export default function Register() {
                         alt="Your Company"
                     />
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Register  your account
+                        Register your account
                     </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" action="#" method="POST">
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
                                 First Name
                             </label>
                             <div className="mt-2">
                                 <input
                                     value={FirstName}
                                     onChange={(e) => setFirstName(e.target.value)}
-                                    name="text"
+                                    name="firstName"
                                     type="text"
-                                    autoComplete="Name"
+                                    autoComplete="given-name"
                                     required
-                                    className="block px-2  w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
                                 Last Name
                             </label>
                             <div className="mt-2">
                                 <input
                                     onChange={(e) => setLastName(e.target.value)}
                                     value={LastName}
-                                    name="text"
+                                    name="lastName"
                                     type="text"
-                                    autoComplete="Name"
+                                    autoComplete="family-name"
                                     required
                                     className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
@@ -85,19 +87,71 @@ export default function Register() {
                         </div>
                         <div className="mt-2 flex flex-row space-x-4">
                             <label className="flex items-center space-x-1">
-                                <input type="checkbox" name="inter" id="sports" value="sports" checked={interest === 'sports'} onChange={(e) => setInterest(e.target.value)} />
+                                <input
+                                    type="checkbox"
+                                    name="interest"
+                                    id="sports"
+                                    value="sports"
+                                    checked={interests.includes('sports')}
+                                    onChange={(e) => {
+                                        if (e.target.checked) {
+                                            setInterests([...interests, 'sports']);
+                                        } else {
+                                            setInterests(interests.filter((interest) => interest !== 'sports'));
+                                        }
+                                    }}
+                                />
                                 Sports
                             </label>
                             <label className="flex items-center space-x-1">
-                                <input type="checkbox" name="inter" id="politics" value="politics" checked={interest === 'politics'} onChange={(e) => setInterest(e.target.value)} />
+                                <input
+                                    type="checkbox"
+                                    name="interest"
+                                    id="politics"
+                                    value="politics"
+                                    checked={interests.includes('politics')}
+                                    onChange={(e) => {
+                                        if (e.target.checked) {
+                                            setInterests([...interests, 'politics']);
+                                        } else {
+                                            setInterests(interests.filter((interest) => interest !== 'politics'));
+                                        }
+                                    }}
+                                />
                                 Politics
                             </label>
                             <label className="flex items-center space-x-1">
-                                <input type="checkbox" name="inter" id="business" value="business" checked={interest === 'business'} onChange={(e) => setInterest(e.target.value)} />
+                                <input
+                                    type="checkbox"
+                                    name="interest"
+                                    id="business"
+                                    value="business"
+                                    checked={interests.includes('business')}
+                                    onChange={(e) => {
+                                        if (e.target.checked) {
+                                            setInterests([...interests, 'business']);
+                                        } else {
+                                            setInterests(interests.filter((interest) => interest !== 'business'));
+                                        }
+                                    }}
+                                />
                                 Business
                             </label>
                             <label className="flex items-center space-x-1">
-                                <input type="checkbox" name="inter" id="bollywood" value="bollywood" checked={interest === 'bollywood'} onChange={(e) => setInterest(e.target.value)} />
+                                <input
+                                    type="checkbox"
+                                    name="interest"
+                                    id="bollywood"
+                                    value="bollywood"
+                                    checked={interests.includes('bollywood')}
+                                    onChange={(e) => {
+                                        if (e.target.checked) {
+                                            setInterests([...interests, 'bollywood']);
+                                        } else {
+                                            setInterests(interests.filter((interest) => interest !== 'bollywood'));
+                                        }
+                                    }}
+                                />
                                 Bollywood
                             </label>
                         </div>
@@ -115,11 +169,10 @@ export default function Register() {
                                     type="email"
                                     autoComplete="email"
                                     required
-                                    className="px-2  block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
-
                         <div>
                             <div className="flex items-center justify-between">
                                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
